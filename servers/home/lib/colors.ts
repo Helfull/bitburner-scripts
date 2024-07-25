2e335d8f4003a4c68587db7e2ac678cc2a55203a
helfull <admin@helfull.de>
2024-07-25 17:25:06 +0200

    add defineScript helper

---
+++ servers/home/lib/colors.ts
@@ -0,0 +1,111 @@
+class Colors {
+  declare readonly white: Colors;
+  declare readonly black: Colors;
+  declare readonly red: Colors;
+  declare readonly green: Colors;
+  declare readonly yellow: Colors;
+  declare readonly pink: Colors;
+  declare readonly grey: Colors;
+  declare readonly whiteBG: Colors;
+  declare readonly redBG: Colors;
+  declare readonly greenBG: Colors;
+  declare readonly yellowBG: Colors;
+
+  private fgColor: string = '';
+  private bgColor: string = '';
+
+  private styleStack: string[] = [];
+
+  constructor() {
+    addFgColor('black', '0');
+    addFgColor('yellow', '3');
+    addFgColor('pink', '5');
+    addFgColor('grey', ' 244');
+    addFgColor('red', '9');
+    addFgColor('green', '10');
+    addFgColor('white', '15');
+
+    addBgColor('red', '1');
+    addBgColor('green', '2');
+    addBgColor('yellow', '3');
+    addBgColor('white', '15');
+  }
+
+  public wrap(msg: string) {
+    const str = `\x1b[${this.fgColorCode};${this.bgColorCode};${this.styleStack.join(';')}m${msg}\x1b[0m`;
+
+    this.fgColor = '';
+    this.bgColor = '';
+    this.styleStack = [];
+
+    return str;
+  }
+
+  public unwrap(msg: string) {
+    return msg.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
+  }
+
+  private get fgColorCode() {
+    return this.fgColor ? `38;5;${this.fgColor}` : '';
+  }
+
+  private get bgColorCode() {
+    return this.bgColor ? `48;5;${this.bgColor}` : '';
+  }
+
+  public static unwrap(msg: string) {
+    return msg.replace(/\u001b[[(?);]{0,2}(;?\d)*./g, '');
+  }
+
+  public fg(color: string) {
+    this.fgColor = color;
+    return this;
+  }
+
+  public bg(color: string) {
+    this.bgColor = color;
+    return this;
+  }
+
+  get bold() {
+    this.styleStack.push('1');
+    return this;
+  }
+
+  get underline() {
+    this.styleStack.push('4');
+    return this;
+  }
+
+  get italic() {
+    this.styleStack.push('3');
+    return this;
+  }
+}
+
+function addFgColor(name: string, color: string) {
+  if (Object.hasOwn(Colors.prototype, name)) return;
+  Object.defineProperty(Colors.prototype, name, {
+    get() {
+      return this.fg(color);
+    },
+  });
+}
+
+function addBgColor(name: string, color: string) {
+  if (Object.hasOwn(Colors.prototype, name + 'BG')) return;
+  Object.defineProperty(Colors.prototype, name + 'BG', {
+    get() {
+      return this.bg(color);
+    },
+  });
+}
+
+const Color = new Colors();
+export { Color };
+export async function main(ns: NS) {
+  ns.tprint(Color.red.whiteBG.wrap('Hello, World!'));
+  ns.tprint(Color.red.whiteBG.underline.bold.wrap('Hello, World!'));
+  ns.tprint(Color.red.whiteBG.wrap('Hello, World!'));
+  ns.tprint(Color.unwrap(Color.underline.bold.red.whiteBG.wrap('Hello, World!')));
+}
---
+++ servers/home/lib/flags.ts
@@ -0,0 +1,110 @@
+import { Color } from '@lib/colors';
+
+type ScriptArg = string | number | boolean;
+
+type ScriptDefinition<T extends FlagsDefinition> = {
+  description: string;
+  flags: T;
+};
+
+type Flag<T = any> = {
+  description: string;
+  defaultValue: T;
+};
+
+type FlagsDefinition = {
+  [K: string]: Flag;
+};
+
+type DefaultFlags = {
+  [K in keyof typeof defaultFlags]: {
+    description: string;
+    defaultValue: (typeof defaultFlags)[K]['defaultValue'];
+  };
+};
+
+type MapFlagsArray<T extends FlagsDefinition> = {
+  [P in keyof T]: T[P]['defaultValue'];
+};
+
+type DefaultFlagsMapped = MapFlagsArray<DefaultFlags>;
+
+type FlagsEvaluated<T extends FlagsDefinition> = MapFlagsArray<T> & DefaultFlagsMapped;
+
+const defaultFlags = {
+  help: {
+    description: 'Displays this help message, providing detailed information about script usage.',
+    defaultValue: false,
+  },
+  cli: {
+    description: 'Enables command-line interface mode, directing output to the terminal.',
+    defaultValue: false,
+  },
+  win: {
+    description: 'Opens script logs in a new window, similar to the --tail option for enhanced readability.',
+    defaultValue: false,
+  },
+};
+
+export function defineScript<T extends FlagsDefinition>(ns: NS, definition: ScriptDefinition<T>): FlagsEvaluated<T> {
+  const args = getFlags(ns, definition.flags);
+
+  setupTail(ns, args);
+
+  printHelp(ns, args, definition);
+
+  return args;
+}
+
+function setupTail<T extends FlagsDefinition>(ns: NS, args: FlagsEvaluated<T>) {
+  ns.disableLog('ALL');
+
+  if (args.win) {
+    ns.tail();
+  }
+}
+
+function printHelp<T extends FlagsDefinition>(ns, args: FlagsEvaluated<T>, definition: ScriptDefinition<T>) {
+  function printSection(ns: any, title: string, content: string) {
+    ns.tprintf(Color.yellow.wrap(`=== ${title} ===\n`));
+    ns.tprintf(content + '\n\n');
+  }
+
+  if (args.help) {
+    const scriptName = Color.pink.wrap(ns.getScriptName());
+    ns.tprintf(Color.green.wrap(`\n********** Help for ${scriptName} **********\n`));
+
+    ns.tprintf(
+      Color.white.wrap(
+        `Welcome to the help section for ${scriptName}. Here's everything you need to know to get started:\n\n`,
+      ),
+    );
+
+    const formatFlag = ([key, { description, defaultValue }]) =>
+      `  ${Color.red.wrap('--' + key)}: (default: ${Color.grey.wrap(defaultValue)})\n    ${description}`;
+
+    const customFlagsHelpText = Object.entries(definition.flags).map(formatFlag).join('\n');
+
+    const defaultFlagsHelpText = Object.entries(defaultFlags).map(formatFlag).join('\n');
+
+    printSection(ns, 'Description', definition.description);
+    printSection(ns, 'Flags', customFlagsHelpText);
+    printSection(ns, 'Global Flags', defaultFlagsHelpText);
+
+    ns.tprintf(Color.white.wrap('For more information, visit the official documentation or reach out on the forums.'));
+
+    ns.exit();
+  }
+}
+
+function getFlags<T extends FlagsDefinition>(ns: NS, flagsInput: T): FlagsEvaluated<T> {
+  const combinedFlagsInput: T & typeof defaultFlags = { ...defaultFlags, ...flagsInput };
+
+  const schema: [string, ScriptArg | string[]][] = Object.entries(combinedFlagsInput).map(([key, { defaultValue }]) => [
+    key,
+    defaultValue,
+  ]);
+  const flagsResult = ns.flags(schema);
+
+  return flagsResult as FlagsEvaluated<T>;
+}
