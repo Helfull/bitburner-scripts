2446a2c425ffd1e1045d255c89df7f73dbcba881
helfull <admin@helfull.de>
2024-07-26 18:26:04 +0200

    adjustments to general lib

--- servers/home/cnc/lib.ts
+++ servers/home/cnc/lib.ts
@@ -1,25 +1,22 @@
-import { ScriptArg } from "../../../NetscriptDefinitions";
-import { Color } from "../colors";
-import { config } from "../config";
+import { Color } from '@lib/colors';
+import { ScriptArg } from '../../../NetscriptDefinitions';
+import { config } from '../config';
 
 export type Schema = [string, string | number | boolean | string[]][];
 
 export function setupDefault(ns: NS, schema?: Schema) {
   const args = flags(ns, schema);
-  ns.disableLog("ALL");
+  ns.disableLog('ALL');
   setupTail(ns, args);
 
   return args;
 }
 
 export function flags(ns: NS, schema?: Schema) {
-  return ns.flags([["tail", false], ...(schema || [])]);
+  return ns.flags([['tail', false], ...(schema || [])]);
 }
 
-export function setupTail(
-  ns: NS,
-  args: { [key: string]: ScriptArg | string[] }
-) {
+export function setupTail(ns: NS, args: { [key: string]: ScriptArg | string[] }) {
   if (args.tail) {
     ns.tprintRaw(`Tailing lsogs`);
     ns.tail();
@@ -48,11 +45,11 @@ export function killOldScript(ns: NS, scriptName: string, server: string) {
 }
 
 export function bool(bool: boolean): string {
-  return bool ? Color.green.wrap("Yes") : Color.red.wrap("No");
+  return bool ? Color.green.wrap('Yes') : Color.red.wrap('No');
 }
 
 export function boolBG(bool: boolean): string {
-  return bool ? Color.greenBG.black.wrap("Yes") : Color.redBG.white.wrap(" No");
+  return bool ? Color.greenBG.black.wrap('Yes') : Color.redBG.white.wrap(' No');
 }
 
 type ProgressOptions = {
@@ -66,32 +63,32 @@ export function progressBar(
   ns: NS,
   current: number,
   max: number,
-  options: ProgressOptions = { width: 20, text: false }
+  percent?: number,
+  options: ProgressOptions = { width: 20, text: false },
 ) {
   options.width = options.width || 20;
   options.text = options.text ?? false;
   options.formatter = options.formatter || ns.formatNumber;
   options.reverse = options.reverse || false;
-  const percent = current / max;
-  let barWidth = Math.floor(options.width * percent);
+  percent = percent !== undefined ? percent : current / max;
+  if (isNaN(percent)) percent = 0;
 
-  const leftColor = options.reverse ? Color.red : Color.green;
-  const leftBar = leftColor.wrap(["\uEE04".repeat(barWidth)].join(""));
-  const rightColor = options.reverse ? Color.green : Color.red;
-  const rightBar = rightColor.wrap(
-    ["\uEE01".repeat(options.width - barWidth)].join("")
-  );
+  const fillColor = options.reverse ? 'red' : 'green';
+  const emptyColor = options.reverse ? 'green' : 'red';
 
-  const startCap =
-    percent > 0 ? Color.green.wrap("\uEE03") : Color.red.wrap("\uEE00");
-  const endCap =
-    percent >= 1 ? Color.green.wrap("\uEE05") : Color.red.wrap("\uEE02");
+  let barWidth = Math.floor(options.width * Math.min(1, percent));
+
+  const leftBar = Color[fillColor].wrap(['\uEE04'.repeat(Math.max(0, barWidth))].join(''));
+  const rightBar = Color[emptyColor].wrap(['\uEE01'.repeat(Math.max(0, options.width - barWidth))].join(''));
+
+  const startCap = percent > 0 ? Color[fillColor].wrap('\uEE03') : Color[emptyColor].wrap('\uEE00');
+  const endCap = percent >= 1 ? Color[fillColor].wrap('\uEE05') : Color[emptyColor].wrap('\uEE02');
   const bar = `${startCap}${leftBar}${rightBar}${endCap}`;
 
   if (options.text === false) return `${bar}`;
-  return `${options.formatter(current)}/${options.formatter(max)} (${ns.sprintf(
-    "%.2f%%",
-    percent * 100
+  return `${options.formatter(current || 0)}/${options.formatter(max || 0)} (${ns.sprintf(
+    '%.2f%%',
+    percent * 100,
   )}) ${bar}`;
 }
 
@@ -120,11 +117,7 @@ export function getServers(ns: NS): string[] {
   return servers;
 }
 
-export function printTableObj(
-  ns: NS,
-  tableRows: { [key: string]: any }[],
-  output = ns.tprint
-) {
+export function printTableObj(ns: NS, tableRows: { [key: string]: any }[], output = ns.tprint) {
   if (tableRows.length === 0) return;
 
   const table = {};
@@ -139,41 +132,36 @@ export function printTableObj(
   printTable(ns, table, output);
 }
 
-export function printTable(
-  ns: NS,
-  table: { [key: string]: any[] },
-  output = ns.tprint
-) {
+export function printTable(ns: NS, table: { [key: string]: any[] }, output = ns.tprint) {
+  let strip = (v: string) => v;
+  if (typeof stripColors === 'function') {
+    strip = stripColors;
+  }
+
   const header = Object.keys(table);
-  const cols = Object.values(table).map((col) =>
-    col.map((v) => v?.toString() || "")
-  );
+  const cols = Object.values(table).map((col) => col.map((v) => v?.toString() || ''));
   const rows = cols[0].map((_, i) => cols.map((col) => col[i]));
 
   const colWidths = header.map((columnHeader, i) => {
-    return Math.max(
-      stripColors(columnHeader).length,
-      ...(cols[i]?.map((v) => stripColors(v || "").length) || [])
-    );
+    const header = strip(columnHeader);
+    const headerWidth = header.length;
+
+    const rowsStr = rows.map((row) => strip(row[i] || ''));
+    const colsWidth = rowsStr.map((row) => row.length);
+
+    const result = Math.max(headerWidth, ...(colsWidth || []));
+    return result;
   });
+
+  const makeCell = (v: string, i: number) => ''.padStart(colWidths[i] - strip(v || '').length, ' ') + (v || '');
 
   output(
     [
-      "",
-      header.map((v, i) => v.padStart(colWidths[i], " ")).join(" | "),
-      header.map((_, i) => "".padStart(colWidths[i], "-")).join(" | "),
-      rows
-        .map((row) =>
-          row
-            .map(
-              (v, i) =>
-                "".padStart(colWidths[i] - stripColors(v || "").length, " ") +
-                (v || "")
-            )
-            .join(" | ")
-        )
-        .join("\n"),
-    ].join("\n")
+      '',
+      header.map((v, i) => makeCell(v, i)).join(' | '),
+      header.map((_, i) => ''.padStart(colWidths[i], '-')).join(' | '),
+      rows.map((row) => row.map((v, i) => makeCell(v, i)).join(' | ')).join('\n'),
+    ].join('\n'),
   );
 }
 
@@ -182,7 +170,7 @@ export function weight(ns: NS, server: string) {
   if (!server) return 0;
 
   // Don't ask, endgame stuff
-  if (server.startsWith("hacknet-node")) return 0;
+  if (server.startsWith('hacknet-node')) return 0;
 
   // Get the player information
   const player = ns.getPlayer();
@@ -200,12 +188,11 @@ export function weight(ns: NS, server: string) {
   let weight = (so.moneyMax || 0) / (so.minDifficulty || 1);
 
   // If we have formulas, we can refine the weight calculation
-  if (ns.fileExists("Formulas.exe")) {
+  if (ns.fileExists('Formulas.exe')) {
     // We use weakenTime instead of minDifficulty since we got access to it,
     // and we add hackChance to the mix (pre-formulas.exe hack chance formula is based on current security, which is useless)
     weight =
-      ((so.moneyMax || 0) / ns.formulas.hacking.weakenTime(so, player)) *
-      ns.formulas.hacking.hackChance(so, player);
+      ((so.moneyMax || 0) / ns.formulas.hacking.weakenTime(so, player)) * ns.formulas.hacking.hackChance(so, player);
   }
   // If we do not have formulas, we can't properly factor in hackchance, so we lower the hacking level tolerance by half
   else if ((so.requiredHackingSkill || 0) > player.skills.hacking / 2) return 0;
